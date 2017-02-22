79be3d667f893d529c263229d64de5f688f65898
elijahchen <code.elijahchen@gmail.com>
2017-02-22 18:31:24 -0500

    - Completed core functionality of the app

--- scorekeeper.js
+++ scorekeeper.js
@@ -1,8 +1,12 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
+var resetButton = document.getElementById("reset");
 
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+var numInput = document.querySelector("input[type='number'");
+var winningScoreDisplay = document.querySelector("p span");
+var p = document.querySelector("p");
 
 var p1Score = 0;
 var p2Score = 0;
@@ -14,6 +18,7 @@ p1Button.addEventListener("click", function(){
 	if(!gameOver){
 		p1Score++;
 		if(p1Score === matchSet){
+			p1Display.classList.add("winner");
 			gameOver = true;
 		}
 		p1Display.textContent = p1Score;
@@ -24,8 +29,28 @@ p2Button.addEventListener("click", function(){
 	if(!gameOver){
 		p2Score++;
 		if(p2Score === matchSet){
+			p2Display.classList.add("winner");
 			gameOver = true;
 		}
 		p2Display.textContent = p2Score;
 	}
+});
+
+resetButton.addEventListener("click", function(){
+	reset();
+});
+
+function reset(){
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = p1Score;
+	p2Display.textContent = p2Score;
+	p1Display.classList.remove("winner");
+	p2Display.classList.remove("winner");
+	gameOver = false;
+}
+
+numInput.addEventListener("change", function(){
+	winningScoreDisplay.textContent = this.value;
+	matchSet = Number(this.value);
 });
